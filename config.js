999d519452b4dc772ca5a10c7f6afce6f4c91e19
securesix <43510195+securesix@users.noreply.github.com>
2021-05-15 21:06:25 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-var api = 'https://api.cybersecuresix.com';
-var apiList = ["http://65.21.159.250:8201", "http://51.15.252.228:8201"];
-=======
 var api = 'https://api.cybersecuresix.com';
 var apiList = ["http://65.21.159.250:8201", "http://65.21.159.250:8201"];
->>>>>>> 8218cbc1c74a268ef030d247b20f8588b1c8c672
-
 var blockTargetInterval = 60;
 var coinUnits = 100;
 var symbol = 'SSIX';
@@ -16,12 +10,5 @@ var nightTheme = "css/dark.css";
 var addressPattern = new RegExp("^S6[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{94}$");
 // pools stats by MainCoins
 var poolsStat = 
-<<<<<<< HEAD
-	[];
-=======
-	[
-		
-    ];
->>>>>>> 8218cbc1c74a268ef030d247b20f8588b1c8c672
 
 	
