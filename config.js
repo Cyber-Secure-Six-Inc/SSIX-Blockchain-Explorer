cf8571a17124f01e9df8a599559cf535db05437d
KonradBaj <konradbaj@gmail.com>
2021-05-15 16:51:15 -0400

    Changes 2

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
-var api = 'https://wallet.cybersecuresix.com/services/node_web';
-var apiList = ["http://65.21.159.250:8200", "http://51.15.252.228:32348"];
+var api = 'https://api.cybersecuresix.com/';
+var apiList = ["http://65.21.159.250:8201", "http://51.15.252.228:8201"];
 
-var blockTargetInterval = 240;
-var coinUnits = 10;
+var blockTargetInterval = 60;
+var coinUnits = 100;
 var symbol = 'SSIX';
 var refreshDelay = 30000;
 var blocksPerPage = 20;
 var whiteTheme = "css/light.css";
 var nightTheme = "css/dark.css";
-var addressPattern = new RegExp("^K[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{94}$");
+var addressPattern = new RegExp("^S6[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{94}$");
 // pools stats by MainCoins
 var poolsStat = 
-	[
-		["karbo.hashvault.pro", "https://karbo.hashvault.pro/api/pool/stats"],
-		["cnpool.cc/krb", "https://cnpool.cc/api/krb/stats"]
-    ];
+	[];
 
 	
