5ca09159e7d87a8f2ab48fa5a36f91ae5954ae81
KonradBaj <konradbaj@gmail.com>
2021-05-15 16:43:16 -0400

    Changes 1

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $node = 'https://karbo.club/services/node_web';
 return array(
 	'api' => $node,
-	'blockTargetInterval' => 240,
-	'coinUnits' => 1000000000000,
-	'coinDecimals' => 12
+	'blockTargetInterval' => 60,
+	'coinUnits' => 10,
+	'coinDecimals' => 2
 );
